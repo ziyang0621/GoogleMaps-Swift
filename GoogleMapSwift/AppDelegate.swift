83088dd3c2be7bf573c996dac3750cc2986770d1
ziyang <ziyang0621@gmail.com>
2015-02-10 09:59:29 -0800

    project update

--- GoogleMapSwift/AppDelegate.swift
+++ GoogleMapSwift/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyDx_z-K-2qQE9iWhcohR5ho9D2_Otb7FZU");
+        GMSServices.provideAPIKey("YOUR_API_KEY");
         return true
     }
 
